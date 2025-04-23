5d74da03381fe02030da215aabd9cf36258c2d2a
Paulen101 <2023225chhun@aupp.edu.kh>
2025-04-23 17:20:05 +0700

    update

--- user-service/index.js
+++ user-service/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const connectToDb = require('../dbconnect');
+const connectToDb = require('./dbconnect');
 const User = require('./models/user');
 
 const app = express();
