43fab3d6a0ad423064471d4814625db6537a5890
Paulen101 <2023225chhun@aupp.edu.kh>
2025-04-23 15:36:01 +0700

    update

--- user-service/models/user.js
+++ user-service/models/user.js
@@ -4,13 +4,10 @@ const bcrypt = require('bcryptjs');  // Import bcrypt for password hashing
 // Define the user schema
 const userSchema = new Schema({
   name:  { 
-    type: String, 
-    required: true 
+    type: String, required: true 
   },
   email: { 
-    type: String, 
-    required: true, 
-    unique: true 
+    type: String, required: true, unique: true 
   },
   password: { 
     type: String, 
@@ -20,13 +17,9 @@ const userSchema = new Schema({
 
 // Password hashing middleware
 userSchema.pre('save', async function(next) {
-  // Only hash the password if it has been modified (or is new)
   if (this.isModified('password')) {
-    // Hash the password with bcrypt, 10 salt rounds
     this.password = await bcrypt.hash(this.password, 10);
   }
   next();  
 });
-
-// Export the User model
 module.exports = model('User', userSchema);
